8e02e4a6c68e275926107dacee0b83289d146830
Mohamed Alabasy <eng.mohamed.alabasy@gmail.com>
2023-03-09 16:00:06 +0200

    add sendEvent

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Sse } from '@nestjs/common';
+import { interval, map, Observable, } from 'rxjs';
 import { AppService } from './app.service';
 
+interface messageEvent {
+  data: string;
+}
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Sse('event')
+  sendEvent(): Observable<messageEvent> {
+    return interval(1000).pipe(map((num) => ({
+      data: 'hello world' + num,
+    })))
   }
 }
